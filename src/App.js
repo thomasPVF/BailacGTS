f49e2cea576cff648151a61cb7bb483363060aeb
thomasPVF <programmincl@gmail.com>
2024-05-13 12:48:55 -0400

    usuario

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [idUsuario, setIdUsuario] = useState('');
+  const [infoUsuario, setInfoUsuario] = useState(null);
+
+  const handleInputChange = (event) => {
+    setIdUsuario(event.target.value);
+  };
+
+  const handleBuscarUsuario = () => {
+    fetch(`https://reqres.in/api/users/${idUsuario}`)
+      .then(response => response.json())
+      .then(data => {
+        setInfoUsuario(data);
+      })
+      .catch(error => {
+        console.error('Error al buscar usuario:', error);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="bp">
+          <b><h3>Buscador de persona</h3></b>
+          <label htmlFor="usuario"> Ingrese un número para Buscar</label>
+
+          <div>
+            <div>
+              <input
+                type="text"
+                placeholder="Ingrese ID de usuario"
+                value={idUsuario}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div>
+              <button onClick={handleBuscarUsuario}>Buscar Usuario</button>
+            </div>
+          </div>
+
+          {infoUsuario && (
+            <div>
+              <h4>Información del Usuario:</h4>
+              <p>ID: {infoUsuario.data.id}</p>
+              <p>Nombre: {infoUsuario.data.first_name}</p>
+              <p>Apellido: {infoUsuario.data.last_name}</p>
+              <img src={infoUsuario.data.avatar} alt="Avatar del usuario" />
+            </div>
+          )}
+
+        </div>
+        {infoUsuario && (
+          <div className="avatar-container">
+            <img src={infoUsuario.data.avatar} alt="Avatar del usuario" />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
